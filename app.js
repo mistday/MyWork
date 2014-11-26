fc95f6bae1fbe983f411a89b10fa411a05539f9f
Dmitry Golofaev <mistday@yandex.ru>
2014-11-26 21:49:43 +0300

    Create simple server on nodejs

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express'),
+	app = express();
+
+app.get('/', function(req,res) {
+	res.send('Hello World!');
+});
+
+var server = app.listen(3000, function() {
+	var host = server.address().address,
+		port = server.address().port;
+
+	console.log('App listening at http://%s:%s', host, port);
+});
