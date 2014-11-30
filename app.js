7c3670ba8a31e2c13a5f09db0e2f295e76ee74fd
Dmitry Golofaev <mistday@yandex.ru>
2014-11-30 12:55:39 +0300

    create test video page, references to them. Create 404 page and functional for her. Add style for button

--- app.js
+++ app.js
@@ -4,10 +4,18 @@ var express = require('express'),
 
 app.set('view engine', 'jade');
 
-app.use("/css", express.static(__dirname+'/templates/css'));
+app.use("*/css", express.static(__dirname+'/templates/css'));
 
-app.get('/', function(req,res) {
+app.get('/', function(req, res) {
 	res.render(__dirname+'/templates/index');
+});
+
+app.get('/video', function(req, res) {
+	res.render(__dirname+'/templates/video');
+});
+
+app.get('*', function(req, res) {
+	res.status(404).render(__dirname+'/templates/404');
 });
 
 var server = app.listen(3000, function() {
