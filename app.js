3639c5e741b5d6fd001690849614a82aa26cf9d6
Dmitry Golofaev <mistday@yandex.ru>
2014-12-07 16:57:18 +0300

    create simple video stream and change style

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express'),
 	jade = require('jade'),
 	app = express();
 
+var fs = require('fs'),
+	movie_mp4;
 app.set('view engine', 'jade');
 
 app.use("*/css", express.static(__dirname+'/templates/css'));
@@ -13,6 +15,39 @@ app.get('/', function(req, res) {
 app.get('/video', function(req, res) {
 	res.render(__dirname+'/templates/video');
 });
+
+app.get('/movies/*', function(req, res) {
+
+	fs.readFile('movies/GoPro3.mp4', function(err, data){
+		if(err) throw err;
+		movie_mp4 = data;
+	/* begin */
+		var total = movie_mp4.length;
+		console.log(total);
+		
+		var range = req.headers.range;
+		console.log(req.headers);
+
+		var positions = range.replace(/bytes=/, "").split("-");
+
+		var start = parseInt(positions[0], 10);
+		var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+
+		var chunksize = (end-start)+1;
+		console.log(chunksize);
+
+
+		res.status(206);
+		res.set({
+			"Content-Range": "bytes " + start + "-" + end + "/" + total, 
+			"Accept-Ranges": "bytes",
+			"Content-Length": chunksize,
+			"Content-Type": "video/mp4"});
+		res.end(movie_mp4.slice(start, end+1), "binary");
+	/* end */
+	});
+});
+
 
 app.get('*', function(req, res) {
 	res.status(404).render(__dirname+'/templates/404');
